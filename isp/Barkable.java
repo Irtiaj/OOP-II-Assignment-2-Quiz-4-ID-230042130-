6fa0ced2622711d0d5fa285eb1d12d3cda9618e7
Irtiaj <irtiaj0541@gmail.com>
2026-05-14 22:26:21 +0600

    Uploading the Fixed code of isp folder

---
+++ isp/Barkable.java
@@ -0,0 +1,5 @@
+package com.codurance.isp;
+
+public interface Barkable extends Animal {
+    void bark();
+}
---
+++ isp/RunnableAnimal.java
@@ -0,0 +1,5 @@
+package com.codurance.isp;
+
+public interface RunnableAnimal extends Animal {
+    void run();
+}
